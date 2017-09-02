ab595e6cadcebcd0d2deabe34cc42a169354143e
Victor Calderon <victor.calderon90@gmail.com>
2017-09-02 00:01:34 -0600

    Reformatted functions

--- Astroweb_post/Astroweb_updates_xmlrpc.py
+++ Astroweb_post/Astroweb_updates_xmlrpc.py
@@ -24,145 +24,130 @@ great care to automatically run this scrip.
 because the wordpress editor's password is in plain text in the script!!
 
 """
+import string
+import datetime
+import time
+import xmlrpclib
+import sys
+import os
+from pandas.tseries.holiday import USFederalHolidayCalendar
 
 
 
-import string, datetime, time, xmlrpclib, sys, os
+def now_dict():
+    """
+    Produces python dictionary with datetime information
+    
+    Returns
+    --------
+    now_dict: python dictionary
+        dictionary with datetime information
+        Keys: 'weekday', 'month', 'day', 'now', 'now_str'
+    """
+    # Get date and time
+    now = datetime.datetime.now()
+    # Format date and time as needed
+    weekday = now.strftime("%a")
+    month   = now.strftime("%b")
+    day     = now.strftime("%d")
+    now_str = now.strftime("%x %a %X")
+    # Converting to dictionary
+    now_dict = {'weekday':weekday, 'month':month, 'day':day, 'now_str':now_str,
+                'now':now}
 
-# get date and time
-now = datetime.datetime.now()
+    return now_dict
 
-# format date and time as needed
-weekday = now.strftime("%a")
-month = now.strftime("%b")
-day = now.strftime("%d")
-
-
-#-----------------------------------------------------------------------------#
 def sendpost(blog_content):
     """
     Use XML-RPC protocol to post to the Astro wordpress site.
     Use python module xmlrpclib to communicate with wordpress XML-RPC.  
     """
-    
-    # wordpress url and accout
+    # Wordpress URL and Account
     wp_url      = "https://as.vanderbilt.edu/astronomy/manage/xmlrpc.php"
     wp_username = os.environ['wp_username']
     wp_password = os.environ['wp_password']
-
-    wp_blogid = "" # wordpress doesn't use this id, so it can be anything. 
-
+    # Blog ID - Wordpress doesn't use this ID, so it can be anything
+    wp_blogid = ""
+    # Status
     status_draft = 0
     status_published = 1
-    
+    # Initializing server
     server = xmlrpclib.ServerProxy(wp_url)
+    # Posing to Wordpress
+    post_id = server.metaWeblog.newPost(wp_blogid,
+                                        wp_username,
+                                        wp_password,
+                                        blog_content,
+                                        status_published)
 
+def sendAstroLunchNotice(now_dict):
+    """
+    Sends notice about `AstroLunch`
 
-    post_id = server.metaWeblog.newPost(wp_blogid, wp_username, wp_password,
-                                        blog_content, status_published)
-
-#-----------------------------------------------------------------------------#
-def sendAstroLunchNotice():
-
-    title = ("AstroLunch, "
-             + weekday + ". " + month + "." + day
-             + " at noon, SC 6333"
-             )
-
+    Parameters
+    ----------
+    now_dict: python dictionary
+        dictionary with datetime information
+        Keys: 'weekday', 'month', 'day', 'now', 'now_str'
+    """
+    # Blog - Title
+    title = 'AstroLunch, {0}. {1}. {2} at noon, sc 6333'.format(
+                    now_dict['weekday'],
+                    now_dict['month'],
+                    now_dict['day'])
+    # Blog - Content
     content = string.join((
         'AstroLunch meets every Tuesday at noon in Stevenson Center 6333. ', 
         '<a href="http://vanderbiltastro.pbworks.com/w/page/12546270/AstroLunch"> Check here for the schedule.</a>'
         ), "\r\n")
-
+    # Blog - Details
     categories = ["Events"]
-
     blog_content = {'title': title, 'description': content, 'categories': categories}
-
+    # Posting Post
     sendpost(blog_content)
 
-#-----------------------------------------------------------------------------#
-def sendAJCNotice():
+def sendAJCNotice(now_dict):
+    """
+    Sends notice about `Astronomy Journal Club`
 
-    title = ("Astronomy Journal Club, "
-             + weekday + ". " + month + "." + day
-             + " at noon, SC 6333"
-             )
-
+    Parameters
+    ----------
+    now_dict: python dictionary
+        dictionary with datetime information
+        Keys: 'weekday', 'month', 'day', 'now', 'now_str'
+    """
+    # Blog - Title
+    title = 'Astronomy Journal Club, {0}. {1}. {2} at noon, SC 6333'.format(
+                    now_dict['weekday'],
+                    now_dict['month'],
+                    now_dict['day'])
+    # Blog - Content
     content = string.join((
         'Astronomy Journal Club meets every Wednesday at noon in Stevenson Center 6333. ', 
         '<a href="http://as.vanderbilt.edu/astronomy/category/journal-club/"> Check here for the full schedule.</a>'
         ), "\r\n")
-
-    categories = ["Events"]
-
-
-    #######################################
-    # This part use the ajc.cron file to automatically put updated info of AJC into the blog.
-    # If it gets weird, feel free to comment out or delete the whole part between "#####"
-    filename = "/home/maoq/www/astro/ajc.cron"
-
-    if os.path.isfile(filename): # check if the file exists
-        # get the time difference between the last modified time and now, in hours
-        timediff = (time.time() - os.path.getmtime(filename)) / 60 / 60
-        # only do this if the file was edited less than 6 days ago
-        if timediff < 24*6: 
-            # read the ajc.cron file and get this week's content
-            file = open(filename, "r")
-            tmpstr = file.readlines()[1:12]
-            new_content = string.join(tmpstr) 
-            content = new_content + content
-    ###########################################
-    
+    # Blog - Details
+    categories   = ["Events"]
     blog_content = {'title': title, 'description': content, 'categories': categories}
-
-    sendpost(blog_content)
-    
-
-#-----------------------------------------------------------------------------# 
-def sendVinoNotice():
-
-    title = ("Vino de Vida, "
-             + weekday + ". " + month + "." + day
-             + " at 4pm, 9th floor hallway"
-             )
-
-    content = string.join((
-        'Vino da Vida happens every Friday at 4pm in the 9th floor hallway. ', 
-        'We have casual discussion over a glass of wine, beer, or soda to unwind and celebrate the end of the week.'
-        ), "\r\n")
-
-    categories = ["Events"]
-    
-    blog_content = {'title': title, 'description': content, 'categories': categories}
-
+    # Posting Post
     sendpost(blog_content)
 
-#-----------------------------------------------------------------------------#
-def sendAstroRWNotice():
+def sendAstroBrewNotice(now_dict):
+    """
+    Sends notice about `AstroBrew`
 
-    title = ("AstroRW, "
-             + weekday + ". " + month + "." + day
-             + " at 2pm, SC Library"
-             )
-
-    content = string.join((
-            'Once a week, we meet in the study room SC3210, and read/write papers for one hour. \n', 
-            'This is a time for us to get together and do the reading and writing of papers that are often "deprioritized" if left to one\'s own devices at their desk. Collectively, we can focus and get ourselves over whatever "hump" lies in from of us. Note that there will be NO presentations; you read/write the material you bring to yourself. '
-            ), '\r\n')
-
-    categories = ["Events"]
-
-    blog_content = {'title': title, 'description': content, 'categories': categories}
-
-    sendpost(blog_content)
-#-----------------------------------------------------------------------------#
-def sendAstroBrewNotice():
-
-    title = ("AstroBrew, "
-             + weekday + ". " + month + "." + day
-             + " at 11am, 9th floor conference room"
-             )
-
+    Parameters
+    ----------
+    now_dict: python dictionary
+        dictionary with datetime information
+        Keys: 'weekday', 'month', 'day', 'now', 'now_str'
+    """
+    # Blog - Title
+    title = 'AstroBrew, {0}. {1}. {2} at 11am, SC 6333'.format(
+                    now_dict['weekday'],
+                    now_dict['month'],
+                    now_dict['day'])
+    # Blog - Content
     content = string.join((
             "AstroBrew is a variant of the AstroCoffee related meetings that the department has had in the past.\n", 
             "The guideline of the AstroBrew are:\n1) 30 min. Maximum.\n2) Agenda is clear: 2-3 papers will be discussed.\n",
@@ -171,147 +156,85 @@ def sendAstroBrewNotice():
             "4) The goal here is simple: maximum information dissemination. The short talks will be 10-15 min presentations of interesting papers.\n", 
             "5) Conversation will be kept on topic. We will get through the main points of the paper quickly.\n6) We will cover a variety of topics.\n"
             ), "\r\n")
-
-    categories = ["Events"]
-
+    # Blog - Details
+    categories   = ["Events"]
     blog_content = {'title': title, 'description': content, 'categories': categories}
-
+    # Posting Post
     sendpost(blog_content)
-#-----------------------------------------------------------------------------#
-def sendAstroChatNotice():
 
-    title = ("AstroChat, "
-             + weekday + ". " + month + "." + day
-             + " at 11am, 9th floor conference room"
-             )
+def sendVinoNotice(now_dict):
+    """
+    Sends notice about `Vino de Vida`
 
+    Parameters
+    ----------
+    now_dict: python dictionary
+        dictionary with datetime information
+        Keys: 'weekday', 'month', 'day', 'now', 'now_str'
+    """
+    # Blog - Title
+    title = 'Vino de Vida, {0}. {1}. {2} at 4pm, 9th floor hallway'.format(
+                    now_dict['weekday'],
+                    now_dict['month'],
+                    now_dict['day'])
+    # Blog - Content
     content = string.join((
-        'AstroChat meets weekly on Friday at 11am in the 9th floor conference room. \n', 
-        'AstroChat is tailored to the grad students, especially junior grad students.  We can come together and chat about the things we learned that week in the other Astro-events. Feel free to bring any question you may have about astronomy and the business of doing astronomy research.  We most likely will not answer every question during AstroChat, but instead we will point you towards someone with whom you can discuss your question offline.\n',
-        'There will be 10min slots available for you to practice presenting a paper you may be thinking of giving to a larger audience at AstroLunch, AJC, or AstroBrew.  Announcements of particular milestones/achievements (no matter how small) in your research or coursework are welcome. Any problems you run into in said areas can be brought up and we can reason together about how to begin to address them.\n', 
-        'In short, this will be a very friendly environment where we are all open about our areas of expertise as well as our areas of ignorance, and thus, no one needs to be shy about not knowing something. The goal is to keep you inspired and motivated as you find yourself trudging through some of the more tedious and perhaps de-moralizing aspects of publishing awesome research!\n'
+        'Vino da Vida happens every Friday at 4pm in the 9th floor hallway. ', 
+        'We have casual discussion over a glass of wine, beer, or soda to unwind and celebrate the end of the week.'
         ), "\r\n")
-    
-    categories = ["Events"]
-
+    # Blog - Details
+    categories   = ["Events"]
     blog_content = {'title': title, 'description': content, 'categories': categories}
-
-    sendpost(blog_content)
-#-----------------------------------------------------------------------------#
-def sendSpecialNotice():
-
-    title = ("Special Talk by , "
-             + weekday + ". " + month + "." + day
-             + " at 11am, SC6501"
-             )
-
-    content = string.join(" ", "\r\n\n")
-
-    categories = ["Events"]
-    
-    blog_content = {'title': title, 'description': content, 'categories': categories}
-
+    # Posting Post
     sendpost(blog_content)
 
-#-----------------------------------------------------------------------------#
-def main():
+def main(now_dict):
+    """
+    Determines current date and sends the expected notice
 
+    Parameters
+    ----------
+    now_dict: python dictionary
+        dictionary with datetime information
+        Keys: 'weekday', 'month', 'day', 'now', 'now_str'
+    """
+    ## Defining `post` strings
+    post_success = '{0}\t Successfully poster. :D\n'.format(now_dict['now_str'])
+    post_ran     = '{0}\t Successfully ran but no post today'.format(
+                        now_dict['now_str'])
+    # Special Holidays
+    post_thanksg = '{0}\t Thanksgiving Break. No Post today. :|\n'.format(
+                        now_dict['now_str'])
+    post_spring  = '{0}\t Spring Break. No Post today. :|\n'.format(
+                        now_dict['now_str'])
+    ## EDIT THIS ONCE PER YEAR
+    thanksg_dates = list(range(20,25))
+    spring_dates  = list(range( 5,10))
+    # Thanksgiving
+    if (now_dict['weekday'] in thanksg_dates) and now_dict['month']=='Nov':
+        sys.stderr.write(post_thanksg)
+        sys.exit(0)
+    # Spring Break
+    if (now_dict['weekday'] in spring_dates) and now_dict['month']=='Mar':
+        sys.stderr.write(post_spring)
+        sys.exit(0)
 
-    # Special Events, edit the dates here, and edit the content in the
-    # function sendSpecialNotice()
-    # Caution! Use double digits for the day, e.g. '05' for 5th.
-    #if month == 'Nov' and day == '05':
-    #    sendSpecialNotice()
-    #    sys.stderr.write("%s\t Special Event posted. :D\n"
-    #                     % now.strftime("%x %a %X"))
-    
-
-    # quit for the Spring Break
-    #if month == 'Mar' and day in ['02', '03', '04', '05', '06']:
-    #    sys.stderr.write("%s\t Spring break. No post today. :|\n"
-    #                     % now.strftime("%x %a %X"))
-    #    sys.exit(0)
-
-    # quit for Thanksgiving
-    # if month == 'Nov' and day in ['24', '25', '26', '27', '28']:
-    #     sys.stderr.write("%s\t Thanksgiving Break. No post today. :|\n"
-    #                      % now.strftime("%x %a %X"))
-    #     sys.exit(0)
-
-
-    # use weekday to decide which notices to send 
-    if weekday == 'Mon':
-        #sendAstroRWNotice()
-        sys.stderr.write("%s\t Successfully posted. :D\n"
-                         % now.strftime("%x %a %X"))
-    elif weekday == 'Tue':
-        sendAstroLunchNotice()
-        sys.stderr.write("%s\t Successfully posted. :D\n"
-                         % now.strftime("%x %a %X"))
-    elif weekday == 'Wed':
-        sendAJCNotice()
-        sys.stderr.write("%s\t Successfully posted. :D\n"
-                         % now.strftime("%x %a %X"))
-    elif weekday == 'Thu':
-        sendAstroBrewNotice()
-        sys.stderr.write("%s\t Successfully posted. :D\n"
-                         % now.strftime("%x %a %X"))
-    elif weekday == 'Fri':
-        #sendAstroChatNotice()
-        sendVinoNotice()
-        sys.stderr.write("%s\t Successfully posted. :D\n"
-                         % now.strftime("%x %a %X"))
+    ## Defining Weekday and deciding which notice to send
+    if now_dict['weekday'] == 'Tues':
+        sendAstroLunchNotice(now_dict)
+        sys.stderr.write(post_success)
+    elif now_dict['weekday'] == 'Wed':
+        sendAJCNotice(now_dict)
+        sys.stderr.write(post_success)
+    elif now_dict['weekday'] == 'Thu':
+        sendAstroBrewNotice(now_dict)
+        sys.stderr.write(post_success)
+    elif now_dict['weekday'] == 'Fri':
+        sendVinoNotice(now_dict)
+        sys.stderr.write(post_success)
     else:
-        sys.stderr.write("%s\t Successfully ran but no post today. :|\n"
-                         % now.strftime("%x %a %X"))
+        sys.stderr.write(post_ran)
 
-
-    
 if __name__ == '__main__':
     main()
-
-
-        
-
-
-
-### --- Archived notices --- ####################################################
-
-# #-----------------------------------------------------------------------------#
-# def sendAstroChatsMonNotice():
-
-#     title = ("AstroChats, "
-#              + weekday + ". " + month + "." + day
-#              + " at 3pm, 9th floor conference room"
-#              )
-
-#     content = string.join((
-#         'AstroChats meets Monday at 3pm and Thursday at 11am in the 9th floor conference room. ', 
-#         '<a href="http://vanderbiltastro.pbworks.com/w/page/57836824/AstroChats"> Check here for the schedule.</a>'
-#         ), "\r\n")
-    
-#     categories = ["Events"]
-
-#     blog_content = {'title': title, 'description': content, 'categories': categories}
-
-#     sendpost(blog_content)
-
-# #-----------------------------------------------------------------------------#
-# def sendAstroChatsThuNotice():
-
-#     title = ("AstroChats, "
-#              + weekday + ". " + month + "." + day
-#              + " at 11am, 9th floor conference room"
-#              )
-
-#     content = string.join((
-#         'AstroChats meets Monday at 3pm and Thursday at 11am in the 9th floor conference room. ', 
-#         '<a href="http://vanderbiltastro.pbworks.com/w/page/57836824/AstroChats"> Check here for the schedule.</a>'
-#         ), "\r\n")
-    
-#     categories = ["Events"]
-
-#     blog_content = {'title': title, 'description': content, 'categories': categories}
-
-#     sendpost(blog_content)
 
