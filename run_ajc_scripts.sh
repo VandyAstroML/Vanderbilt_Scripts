69fd6395df987e1710229a852057171fa56b9693
Victor Calderon <victor.calderon90@gmail.com>
2017-09-12 17:38:51 -0500

    Other way of activating environment

--- run_ajc_scripts.sh
+++ run_ajc_scripts.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ## Description: Runs the commands to run scripts
 ##
@@ -7,12 +7,12 @@ env_name='vandyscripts'
 my_dir=`pwd`
 # echo "my_dir: ${my_dir}"
 # echo "source activate ${env_name}"
-source activate ${env_name}
+# source activate ${env_name}
 # conda env list
 # Run Scripts
 # echo "python ${my_dir}/Astroweb_post/Astroweb_updates_xmlrpc.py >> ${my_dir}/Astroweb_post/updatelog2 2>&1"
-python ${my_dir}/Astroweb_post/Astroweb_updates_xmlrpc.py >> ${my_dir}/Astroweb_post/updatelog2 2>&1
+source activate ${env_name} && python ${my_dir}/Astroweb_post/Astroweb_updates_xmlrpc.py >> ${my_dir}/Astroweb_post/updatelog2 2>&1 && source deactivate
 # echo   "python ${my_dir}/AJC_Scheduler/AJC_Reminders.py >> ${my_dir}/AJC_Scheduler/ajc_log 2>&1"
-python ${my_dir}/AJC_Scheduler/AJC_Reminders.py >> ${my_dir}/AJC_Scheduler/ajc_log 2>&1
+source activate ${env_name} && python ${my_dir}/AJC_Scheduler/AJC_Reminders.py >> ${my_dir}/AJC_Scheduler/ajc_log 2>&1 && source deactivate
 # Deactivating Environment
-source deactivate
+# source deactivate
