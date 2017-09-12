94995f52e526011075955785349082a06d1a9d0c
Victor Calderon <victor.calderon90@gmail.com>
2017-09-12 16:47:14 -0500

    Added `my_dir` to filepath

--- run_ajc_scripts.sh
+++ run_ajc_scripts.sh
@@ -4,13 +4,15 @@
 ##
 # Activate Environment
 env_name='vandyscripts'
-echo "source activate ${env_name}"
+my_dir=`pwd`
+# echo "my_dir: ${my_dir}"
+# echo "source activate ${env_name}"
 source activate ${env_name}
 conda env list
 # Run Scripts
-# echo "python ./Astroweb_post/Astroweb_updates_xmlrpc.py >> ./Astroweb_post/updatelog2 2>&1"
-# python ./Astroweb_post/Astroweb_updates_xmlrpc.py >> ./Astroweb_post/updatelog2 2>&1
-# echo   "python ./AJC_Scheduler/AJC_Reminders.py >> ./AJC_Scheduler/ajc_log 2>&1"
-python ./AJC_Scheduler/AJC_Reminders.py >> ./AJC_Scheduler/ajc_log 2>&1
+# echo "python ${my_dir}/Astroweb_post/Astroweb_updates_xmlrpc.py >> ${my_dir}/Astroweb_post/updatelog2 2>&1"
+python ${my_dir}/Astroweb_post/Astroweb_updates_xmlrpc.py >> ${my_dir}/Astroweb_post/updatelog2 2>&1
+# echo   "python ${my_dir}/AJC_Scheduler/AJC_Reminders.py >> ${my_dir}/AJC_Scheduler/ajc_log 2>&1"
+# python ${my_dir}/AJC_Scheduler/AJC_Reminders.py >> ${my_dir}/AJC_Scheduler/ajc_log 2>&1
 # Deactivating Environment
 source deactivate
