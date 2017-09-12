87ec5b2f9616a94cebf72263f9b4f972157aeb3e
Victor Calderon <victor.calderon90@gmail.com>
2017-09-12 12:54:06 -0500

    Added file to run python scripts

---
+++ run_ajc_scripts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Description: Runs the commands to run scripts
+##
+# Activate Environment
+env_name='vandyscripts'
+echo "source activate $(env_name)"
+source activate $(env_name)
+conda env list
+# Run Scripts
+echo "python ./Astroweb_post/Astroweb_updates_xmlrpc.py >> ./Astroweb_post/updatelog2 2>&1"
+# python ./Astroweb_post/Astroweb_updates_xmlrpc.py >> ./Astroweb_post/updatelog2 2>&1
+echo "python ./AJC_Scheduler/AJC_Reminders.py >> ./AJC_Scheduler/ajc_log 2>&1"
+# python ./AJC_Scheduler/AJC_Reminders.py >> ./AJC_Scheduler/ajc_log 2>&1
+# Deactivating Environment
+source deactivate
