057fc2f68025ec7b3b10ad53f9de39e4280e36ea
Victor Calderon <victor.calderon90@gmail.com>
2017-09-13 23:21:53 -0500

    Added environment name

--- run_ajc_scripts.sh
+++ run_ajc_scripts.sh
@@ -8,6 +8,8 @@ proj_dir=`dirname $0`
 # Type of OS
 ostype=`uname`
 echo "$ostype"
+# Environment name
+ENV_NAME="vandyscripts"
 
 ## Description: Runs the commands to run scripts
 ##
@@ -19,14 +21,19 @@ else
     source $home_dir/.bashprofile
     # echo "source bashprofile"
 fi
-# Sourcing activate and deactivate
+# Activating Environment
 activate=`which activate`
+source $activate $ENV_NAME
+# cd into Script Directory
+cd $proj_dir
+# Run Astro Website Script
+python Astroweb_post/Astroweb_updates_xmlrpc.py >> Astroweb_post/updatelog2 2>&1
+# Run AJC Script
+python AJC_Scheduler/AJC_Reminders.py >> AJC_Scheduler/ajc_log 2>&1
+# Deactivating Environment
 deactivate=`which deactivate`
-echo "$activate"
-echo "$deactivate"
-
-source $activate "vandyscripts"
-echo "$CONDA_DEFAULT_ENV"
+source $deactivate
+# echo "$CONDA_DEFAULT_ENV"
 
 
 
